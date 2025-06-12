5e251e79a68a669700791654065226bc42ddd94a
bluehj777 <huajia@jiahua.org.cn>
2025-06-12 13:41:05 +0800

    Update Handles.sh

--- Scripts/Handles.sh
+++ Scripts/Handles.sh
@@ -89,8 +89,6 @@ fi
 RUST_FILE=$(find ../feeds/packages/ -maxdepth 3 -type f -wholename "*/rust/Makefile")
 if [ -f "$RUST_FILE" ]; then
 	echo " "
-
 	sed -i 's/ci-llvm=true/ci-llvm=false/g' $RUST_FILE
-
 	cd $PKG_PATH && echo "rust has been fixed!"
 fi
